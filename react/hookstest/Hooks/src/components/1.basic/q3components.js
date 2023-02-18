6759fe24fadbf165d5c77bca9c0ce73d96754fac
taeginote <ab315601@naver.com>
2023-02-19 03:04:41 +0900

    Add : q3 풀이 하였습니다

--- react/hookstest/Hooks/src/components/1.basic/q3components.js
+++ react/hookstest/Hooks/src/components/1.basic/q3components.js
@@ -1,4 +1,19 @@
-function Q3components() {
-    return <div> 🏃‍♂️ 줄넘기 ... ing </div>;
+import { useEffect, useRef, useState } from "react";
+
+function Q3components({ count, setCount }) {
+    const countRef = useRef(0);
+
+    useEffect(() => {
+        countRef.current = setInterval(() => {
+            setCount((prev) => prev + 2);
+            console.log(count);
+        }, 2000);
+        return () => {
+            clearInterval(countRef.current);
+            setCount(0);
+        };
+    }, []);
+
+    return <div>🏃‍♂️ 줄넘기 ... ing </div>;
 }
 export default Q3components;
