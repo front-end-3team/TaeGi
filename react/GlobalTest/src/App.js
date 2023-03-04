b085ae6912e38a617aea9770ff8c5f68c5b5b295
taeginote <ab315601@naver.com>
2023-03-05 01:40:31 +0900

    ignore delete

--- react/GlobalTest/src/App.js
+++ react/GlobalTest/src/App.js
@@ -3,11 +3,11 @@ import { RouterProvider } from "react-router-dom";
 import router from "./adapters/router";
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
 }
-
+//
 export default App;
